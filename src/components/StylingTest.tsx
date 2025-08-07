089aef2893ccf972eee59a75cfccb372da26494c
Jake Andrews <jake.andrews@ext.ons.gov.uk>
2025-08-07 17:06:24 +0100

    Create high-level design variables with responsivity according to breakpoints using sass mixins

---
+++ src/components/StylingTest.tsx
@@ -0,0 +1,10 @@
+export default function StylingTestComponent() {
+  return (
+    <>
+      <h1 className="heading-xl">Heading-xl example</h1>
+      <h1 className="heading-l">Heading-l example</h1>
+      <h1 className="heading-m">Heading-m example</h1>
+      <h1 className="heading-s">Heading-xl example</h1>
+    </>
+  );
+}
