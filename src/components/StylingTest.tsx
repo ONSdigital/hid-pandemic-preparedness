0b9f0eff48d506ebd12f50bb9e2381f6c1496ef1
Jake Andrews <jake.andrews@ext.ons.gov.uk>
2025-08-08 13:12:12 +0100

    Example spacings add to test component for POC

--- src/components/StylingTest.tsx
+++ src/components/StylingTest.tsx
@@ -5,6 +5,48 @@ export default function StylingTestComponent() {
       <h1 className="heading-l">Heading-l example</h1>
       <h1 className="heading-m">Heading-m example</h1>
       <h1 className="heading-s">Heading-xl example</h1>
+      <div
+        className="spacing-2xs"
+        style={{ background: "green", width: "50%" }}
+      >
+        spacing-2xs example
+      </div>
+      <div
+        className="spacing-xs"
+        style={{ background: "green", width: "50%", marginTop: "10px" }}
+      >
+        spacing-xs example
+      </div>
+      <div
+        className="spacing-sm"
+        style={{ background: "green", width: "50%", marginTop: "10px" }}
+      >
+        spacing-sm example
+      </div>
+      <div
+        className="spacing-md"
+        style={{ background: "green", width: "50%", marginTop: "10px" }}
+      >
+        spacing-md example
+      </div>
+      <div
+        className="spacing-lg"
+        style={{ background: "green", width: "50%", marginTop: "10px" }}
+      >
+        spacing-lg example
+      </div>
+      <div
+        className="spacing-xl"
+        style={{ background: "green", width: "50%", marginTop: "10px" }}
+      >
+        spacing-xl example
+      </div>
+      <div
+        className="spacing-2xl"
+        style={{ background: "green", width: "50%", marginTop: "10px" }}
+      >
+        spacing-2xl example
+      </div>
     </>
   );
 }
