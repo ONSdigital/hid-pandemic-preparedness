e4086659154774d26548d514bdef5335bad5c4ab
Jake Andrews <jake.andrews@ext.ons.gov.uk>
2025-08-14 15:19:56 +0100

    WIP

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,25 @@
 import { defineConfig } from "astro/config";
+import { storyblok } from "@storyblok/astro";
+import { loadEnv } from "vite";
 
-// https://astro.build/config
+const env = loadEnv("", process.cwd(), "STORYBLOK");
+
 export default defineConfig({
   vite: {
     plugins: [],
+  },
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_DELIVERY_API_TOKEN,
+      apiOptions: {
+        region: "eu",
+      },
+    }),
+  ],
+  components: {
+    page: "storyblok/Page",
+    grid: "storyblok/Grid",
+    feature: "storyblok/Feature",
+    teaser: "storyblok/Teaser",
   },
 });
