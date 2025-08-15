94e106b043afb6482f7ec06510760d99cd0a8e42
JonnyAdcockAND <jonathan.adcock@ext.ons.gov.uk>
2025-08-15 14:21:54 +0100

    added spacing story

---
+++ src/styles/elements/spacing.stories.tsx
@@ -0,0 +1,58 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import "./_spacing.scss";
+
+const meta = {
+  title: "Elements/Spacing",
+} satisfies Meta;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Spacing = {
+  render: () => (
+    <div>
+      <div
+        className="spacing-2xs"
+        style={{ background: "green", width: "50%" }}
+      >
+        spacing-2xs example
+      </div>
+      <div
+        className="spacing-xs"
+        style={{ background: "green", width: "50%", marginTop: "10px" }}
+      >
+        spacing-xs example
+      </div>
+      <div
+        className="spacing-sm"
+        style={{ background: "green", width: "50%", marginTop: "10px" }}
+      >
+        spacing-sm example
+      </div>
+      <div
+        className="spacing-md"
+        style={{ background: "green", width: "50%", marginTop: "10px" }}
+      >
+        spacing-md example
+      </div>
+      <div
+        className="spacing-lg"
+        style={{ background: "green", width: "50%", marginTop: "10px" }}
+      >
+        spacing-lg example
+      </div>
+      <div
+        className="spacing-xl"
+        style={{ background: "green", width: "50%", marginTop: "10px" }}
+      >
+        spacing-xl example
+      </div>
+      <div
+        className="spacing-2xl"
+        style={{ background: "green", width: "50%", marginTop: "10px" }}
+      >
+        spacing-2xl example
+      </div>
+    </div>
+  ),
+} satisfies Story;
--- src/styles/elements/typography.stories.tsx
+++ src/styles/elements/typography.stories.tsx
@@ -2,19 +2,19 @@ import type { Meta, StoryObj } from "@storybook/react";
 import "./_typography.scss";
 
 const meta = {
-  title: "Typography/Headings",
+  title: "Elements/Typography",
 } satisfies Meta;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Headings = {
+export const Typography = {
   render: () => (
-    <div style={{ padding: "1rem" }}>
-      <h1 className="heading-xl">Heading XL - The quick brown fox</h1>
-      <h2 className="heading-l">Heading L - Jumps over the lazy dog</h2>
-      <h3 className="heading-m">Heading M - Lorem ipsum dolor sit amet</h3>
-      <h4 className="heading-s">Heading S - Consectetur adipiscing elit</h4>
+    <div>
+      <h1 className="heading-xl">Heading-xl example</h1>
+      <h1 className="heading-l">Heading-l example</h1>
+      <h1 className="heading-m">Heading-m example</h1>
+      <h1 className="heading-s">Heading-xl example</h1>
     </div>
   ),
 } satisfies Story;
