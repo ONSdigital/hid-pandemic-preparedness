9c52a58c8321e4be5a4da88cb0afa83357f104e4
Jake Andrews <jake.andrews@ext.ons.gov.uk>
2025-08-18 18:38:35 +0100

    Scss and font family added and brought in for use in Header component

---
+++ src/components/navigation/Header/Header.tsx
@@ -0,0 +1,16 @@
+import "./header.scss";
+
+export default function Header() {
+  return (
+    <div className="header-container">
+      <div className="header-content">
+        <p className="heading-s">Home</p>
+        <h1 className="heading-l">Resilience Through Data</h1>
+        <p className="heading-s">
+          Empowering global National Statistics Offices with the skills and
+          evidence to shape effective pandemic response policy.
+        </p>
+      </div>
+    </div>
+  );
+}
