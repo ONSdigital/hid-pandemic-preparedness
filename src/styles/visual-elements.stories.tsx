d50d41f45f3fb67c8c6515f96c21ac7dfdb0338e
JonnyAdcockAND <jonathan.adcock@ext.ons.gov.uk>
2025-08-19 12:28:41 +0100

    updated stories and variables

--- src/styles/visual-elements.stories.tsx
+++ src/styles/visual-elements.stories.tsx
@@ -7,68 +7,59 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
+interface ColorSwatchProps {
+  backgroundColorVar: string;
+  inverseText?: boolean;
+}
+
+// React component to show colours
+const ColorSwatch: React.FC<ColorSwatchProps> = ({
+  backgroundColorVar,
+  inverseText = false,
+}) => {
+  const backgroundColorValue = `var(${backgroundColorVar})`;
+  const textColorValue = inverseText ? "var(--color-text-inverse)" : undefined;
+
+  return (
+    <p
+      style={{
+        backgroundColor: backgroundColorValue,
+        border: "solid",
+        borderColor: "var(--color-text-default)",
+        borderRadius: "var(--corner-radius-default)",
+        borderWidth: "1px",
+        color: textColorValue,
+        padding: "2px",
+        paddingLeft: "8px",
+      }}
+    >
+      <code>{backgroundColorVar}</code>
+    </p>
+  );
+};
+
 export const Colour = {
   name: "Colour",
   render: () => (
     <div>
       <h1 className="heading-l">Colour</h1>
       <h2 className="heading-s">Text</h2>
-      <p
-        style={{
-          backgroundColor: "var(--text-default)",
-          color: "var(--text-inverse)",
-        }}
-      >
-        --text-default
-      </p>
-      <p
-        style={{
-          backgroundColor: "var(--text-inverse)",
-        }}
-      >
-        --text-inverse
-      </p>
-      <p
-        style={{
-          backgroundColor: "var(--text-accent)",
-          color: "var(--text-inverse)",
-        }}
-      >
-        --text-accent
-      </p>
+      <ColorSwatch backgroundColorVar="--color-text-default" inverseText />
+      <ColorSwatch backgroundColorVar="--color-text-inverse" />
+      <ColorSwatch backgroundColorVar="--color-text-accent" inverseText />
       <hr />
       <h2 className="heading-s">Surface</h2>
-      <p
-        style={{
-          backgroundColor: "var(--surface-default)",
-        }}
-      >
-        --surface-default
-      </p>
-      <p
-        style={{
-          backgroundColor: "var(--surface-subtle)",
-        }}
-      >
-        --surface-subtle
-      </p>
-      <p
-        style={{
-          backgroundColor: "var(--surface-subtle-alt)",
-        }}
-      >
-        --surface-subtle-alt
-      </p>
-      <p
-        style={{
-          backgroundColor: "var(--surface-inverse)",
-          color: "var(--text-inverse)",
-        }}
-      >
-        --surface-inverse
-      </p>
+      <ColorSwatch backgroundColorVar="--color-surface-default" />
+      <ColorSwatch backgroundColorVar="--color-surface-subtle" />
+      <ColorSwatch backgroundColorVar="--color-surface-subtle-alt" />
+      <ColorSwatch backgroundColorVar="--color-surface-inverse" inverseText />
       <hr />
       <h2 className="heading-s">Brand</h2>
+      <ColorSwatch
+        backgroundColorVar="--color-brand-primary-default"
+        inverseText
+      />
+      <ColorSwatch backgroundColorVar="--color-brand-secondary-default" />
       <hr />
     </div>
   ),
