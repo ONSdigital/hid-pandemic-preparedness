3462bd080060445b15891b97e8ebd47902243a16
JonnyAdcockAND <jonathan.adcock@ext.ons.gov.uk>
2025-08-27 12:25:34 +0100

    added onClick placeholder

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -7,11 +7,17 @@ export const Button: FC<ButtonProps> = (props) => {
   // Combine base class and variant class
   const classes = `button button--${props.variant}`.trim();
 
+  const onClick = () => {
+    console.log("clicked!");
+  };
+
   return (
     <button
       aria-label={props.ariaLabel}
       className={classes}
       disabled={props.disabled}
+      onClick={onClick}
+      type={props.type}
     >
       <div className="button__label">
         <props.children />
