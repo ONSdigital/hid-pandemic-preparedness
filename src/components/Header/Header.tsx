8103d2a6714b7276a58f0f53d68433c1f4f28ddf
Jake Andrews <jake.andrews@ext.ons.gov.uk>
2025-09-04 13:16:05 +0100

    Header scss converted to scss module

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,13 @@
 import type { FC } from "react";
 
 import type { HeaderProps } from "./Header.interface";
-import "./Header.scss";
+import styles from "./Header.module.scss";
 import SearchBar from "../SearchBar/SearchBar";
 
 const Header: FC<HeaderProps> = ({ subheading, heading, description }) => {
   return (
     <header>
-      <div className="header-content">
+      <div className={styles["header-content"]}>
         <h2 className="body">{subheading}</h2>
         <h1 className="heading-xl">{heading}</h1>
         <p className="heading-s">{description}</p>
