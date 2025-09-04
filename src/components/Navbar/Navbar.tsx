5fe5f38d1e935518996c01e652d6abcc3d32c52e
JonnyAdcockAND <jonathan.adcock@ext.ons.gov.uk>
2025-09-04 16:33:14 +0100

    added beginnings of mega menu

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,38 @@
 import type { FC } from "react";
+import { useState } from "react";
 import {
   RiArrowDropDownLine,
+  RiArrowDropUpLine,
   RiGlobalLine,
   RiSearchLine,
 } from "@remixicon/react";
 
+import type { NavItem } from "../../types/NavItem";
 import styles from "./Navbar.module.scss";
 import type { NavbarProps } from "./Navbar.interface";
+
+// eslint-disable-next-line no-unused-vars
+const MegaMenu: FC<NavItem[]> = (props) => {
+  return <div className={styles["mega-menu"]}>Blah</div>;
+};
+
+const NavbarItem: FC<NavItem> = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  // Toggles state
+  const toggleOpen = () => setIsOpen((val) => !val);
+
+  return (
+    <li className={styles["nav-item"]}>
+      <a className={styles["nav-link"]} href="#" onClick={toggleOpen}>
+        {props.label}{" "}
+        {props.children &&
+          (isOpen ? <RiArrowDropUpLine /> : <RiArrowDropDownLine />)}
+      </a>
+      {props.children && isOpen && <MegaMenu {...props.children} />}
+    </li>
+  );
+};
 
 export const Navbar: FC<NavbarProps> = (props) => {
   return (
@@ -18,18 +44,7 @@ export const Navbar: FC<NavbarProps> = (props) => {
       </div>
       <ul className={styles["navbar-nav"]}>
         {props.navItems.map((item) => (
-          <li className={styles["nav-item"]}>
-            <a className={styles["nav-link"]} href="#">
-              {item.label} {item.children && <RiArrowDropDownLine />}
-            </a>
-            {/* <ul>
-              {item.links?.map((link) => (
-                <li>
-                  <a href={link.href}>{link.label}</a>
-                </li>
-              ))}
-            </ul> */}
-          </li>
+          <NavbarItem {...item} />
         ))}
       </ul>
       <div>
