f349114bff8d063f75f98e93c087ab84d522062e
JonnyAdcockAND <jonathan.adcock@ext.ons.gov.uk>
2025-09-05 22:01:08 +0100

    styling

--- src/components/MenuList/MenuList.tsx
+++ src/components/MenuList/MenuList.tsx
@@ -1,17 +1,27 @@
+import clsx from "clsx";
 import type { FC } from "react";
+
 import type { MenuListProps } from "./MenuList.interface";
+import styles from "./MenuList.module.scss";
 
 export const MenuList: FC<MenuListProps> = (props) => {
   return (
     <div>
-      <h2>{props.label}</h2>
+      <h2 className={clsx("body", styles["menu-list__label"])}>
+        {props.label}
+      </h2>
       {props.children && (
         <ul>
           {props.children.map((child, index, arr) => (
-            <li key={child.href}>
+            <li
+              className={clsx("body", styles["menu-list__item"])}
+              key={child.href}
+            >
               <a href={child.href}>{child.label}</a>
               {/* Include divider except on last item */}
-              {index !== arr.length - 1 && <hr />}
+              {index !== arr.length - 1 && (
+                <div className={styles["menu-list__divider"]} />
+              )}
             </li>
           ))}
         </ul>
