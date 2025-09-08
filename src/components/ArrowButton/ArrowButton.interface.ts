72fcb50e4ad4fee7a66ebec30fdb37943dfe6c49
JonnyAdcockAND <jonathan.adcock@ext.ons.gov.uk>
2025-09-08 07:19:28 +0100

    deleting ArrowButton component

--- src/components/ArrowButton/ArrowButton.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface ArrowButtonProps {
-  ariaLabel: string;
-  disabled?: boolean;
-  direction: "left" | "right";
-  selected?: boolean;
-  type: "button" | "submit" | "reset";
-  variant: "primary" | "primary-inverse";
-}
