d7e9ace8f7a2a30dfe2e22d7c3361f92e15c1741
JonnyAdcockAND <jonathan.adcock@ext.ons.gov.uk>
2025-09-09 22:24:17 +0100

    updating footer to use bootstrap

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,7 @@
 import type { Preview } from "@storybook/react-vite";
+
 import "../src/styles/index.scss";
+import "../src/styles/bootstrap-5.3.8/dist/js/bootstrap.bundle";
 import "@fontsource-variable/open-sans";
 
 const preview: Preview = {
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import type { FC } from "react";
 
 import type { FooterProps } from "./Footer.interface";
@@ -25,16 +26,83 @@ const FooterColumn: FC<MenuData> = ({ title, links }) => {
 };
 
 // React component for the footer
-export const Footer: FC<FooterProps> = ({
-  columnLeft,
-  columnMid,
-  columnRight,
-}) => {
+export const Footer: FC<FooterProps> = (props) => {
   return (
-    <footer className={styles["footer"]}>
-      <FooterColumn {...columnLeft} />
-      <FooterColumn {...columnMid} />
-      <FooterColumn {...columnRight} />
+    <footer
+      className={clsx(
+        "w-100",
+        "py-4",
+        "bg-primary",
+        "bg-gradient",
+        "text-light",
+      )}
+    >
+      <div className="container">
+        {/* Large screens: 4 columns */}
+        <div className={clsx("row d-none d-md-flex")}>
+          {props.columns.map((column, index) => (
+            <div className="col-md-3" key={`col-${index}`}>
+              <h5>{column.title}</h5>
+              <p>Content for column {index}.</p>
+            </div>
+          ))}
+        </div>
+        {/* Small screens: 3 accordions + 1 column below */}
+        <div className="d-md-none">
+          <div
+            className={clsx("accordion", "accordion-flush", "bg-primary")}
+            id="footerAccordion"
+          >
+            {props.columns
+              .slice(0, props.columns.length - 1)
+              .map((column, index, arr) => (
+                <div
+                  className={clsx(
+                    "accordion-item",
+                    "bg-primary",
+                    "bg-gradient",
+                  )}
+                  key={`accordion-${index}`}
+                >
+                  <h2 className={clsx("accordion-header")}>
+                    <button
+                      className={clsx(
+                        "accordion-button",
+                        "collapsed",
+                        "bg-primary",
+                        "text-light",
+                      )}
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target={`#flush-collapse${index}`}
+                      aria-expanded="false"
+                      aria-controls={`flush-collapse${index}`}
+                    >
+                      {column.title}
+                    </button>
+                  </h2>
+                  <div
+                    id={`flush-collapse${index}`}
+                    className={clsx("accordion-collapse", "collapse")}
+                    data-bs-parent="#footerAccordion"
+                  >
+                    <div className="accordion-body">
+                      Placeholder content for this accordion, which is intended
+                      to demonstrate the <code>.accordion-flush</code> class.
+                      This is the first item’s accordion body.
+                    </div>
+                  </div>
+                </div>
+              ))}
+          </div>
+          {props.columns[props.columns.length - 1] && (
+            <div className="mt-4">
+              <h5>Column 4</h5>
+              <p>Content for column 4.</p>
+            </div>
+          )}
+        </div>
+      </div>
     </footer>
   );
 };
