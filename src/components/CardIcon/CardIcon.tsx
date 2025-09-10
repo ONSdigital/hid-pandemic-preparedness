b86e9721d4710c045f9759cde0cbf1ba9c47e8ca
JonnyAdcockAND <jonathan.adcock@ext.ons.gov.uk>
2025-09-10 17:15:27 +0100

    removing unnecessary div

--- src/components/CardIcon/CardIcon.tsx
+++ src/components/CardIcon/CardIcon.tsx
@@ -24,28 +24,26 @@ const iconMap: Record<string, ReactNode> = {
 export const CardIcon: FC<CardIconProps> = (props) => {
   return (
     <div className={clsx("card", styles["card-icon"], "py-4")}>
-      <div>
-        <div className={clsx("card-body", "text-center")}>
-          <div
-            className={clsx(
-              styles["icon-box"],
-              "rounded",
-              "d-flex",
-              "align-items-center",
-              "justify-content-center",
-              "mx-auto",
-            )}
-          >
-            {iconMap[props.icon]}
-          </div>
-          <h4
-            className={clsx("card-title", "py-3", "heading-s", "border-bottom")}
-          >
-            {props.title}
-          </h4>
-          <p className={clsx("card-text")}>{props.subTitle}</p>
-          <Link href={props.link.href} label={props.link.label} />
+      <div className={clsx("card-body", "text-center")}>
+        <div
+          className={clsx(
+            styles["icon-box"],
+            "rounded",
+            "d-flex",
+            "align-items-center",
+            "justify-content-center",
+            "mx-auto",
+          )}
+        >
+          {iconMap[props.icon]}
         </div>
+        <h4
+          className={clsx("card-title", "py-3", "heading-s", "border-bottom")}
+        >
+          {props.title}
+        </h4>
+        <p className={clsx("card-text")}>{props.subTitle}</p>
+        <Link href={props.link.href} label={props.link.label} />
       </div>
     </div>
   );
