f60bf80a637ac2b831f958de11fe3b32af8b417e
JonnyAdcockAND <jonathan.adcock@ext.ons.gov.uk>
2025-09-11 14:51:39 +0100

    fixed mobile padding and adjusted base font size

--- src/components/MegaMenu/MegaMenu.tsx
+++ src/components/MegaMenu/MegaMenu.tsx
@@ -8,10 +8,10 @@ import styles from "./MegaMenu.module.scss";
 export const MegaMenu: FC<MegaMenuProps> = (props) => {
   return (
     <div className={clsx(styles["mega-menu"], "w-100", "py-4")}>
-      <div className={clsx("container")}>
+      <div className={clsx("container-xl")}>
         <div className={clsx("row", "row-cols-1", "row-cols-lg-5")}>
           {props.navItems.map((navItem) => (
-            <div className={clsx("col")} key={navItem.id}>
+            <div className={clsx("col", "py-2")} key={navItem.id}>
               <ListGroupLinks title={navItem.label} {...navItem} />
             </div>
           ))}
