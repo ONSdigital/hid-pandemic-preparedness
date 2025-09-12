f79cc4cd517da8ae7b59828741924ece5fce9f8a
JonnyAdcockAND <jonathan.adcock@ext.ons.gov.uk>
2025-09-12 17:12:53 +0100

    fixing linting errors

--- src/components/Home/QuickLinks/QuickLinks.stories.tsx
+++ src/components/Home/QuickLinks/QuickLinks.stories.tsx
@@ -1,5 +1,4 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { v4 as uuidv4 } from "uuid";
 
 import menuItems from "../../../content/menuItems.json";
 import type { NavItem } from "../../../types/NavItem";
