567a49b24953f00ccf77f44f779d9d99a38833f4
JonnyAdcockAND <jonathan.adcock@ext.ons.gov.uk>
2025-09-12 17:03:30 +0100

    new QuickLinks component

---
+++ src/components/Home/QuickLinks/QuickLinks.stories.tsx
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { v4 as uuidv4 } from "uuid";
+
+import menuItems from "../../../content/menuItems.json";
+import type { NavItem } from "../../../types/NavItem";
+import { QuickLinks } from "./QuickLinks";
+import type { QuickLinksProps } from "./QuickLinks.interface";
+
+const meta = {
+  component: QuickLinks,
+  title: "Organisms/Home/QuickLinks",
+  parameters: {
+    layout: "fullscreen",
+  },
+  argTypes: {
+    columns: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+} satisfies Meta<typeof QuickLinks>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const quickLinksProps: QuickLinksProps = {
+  title: "Learning resources",
+  // This loads the "learning resources part of the menu items"
+  navItems: menuItems[1].children as NavItem[],
+};
+
+export const QuickLinksStory = {
+  name: "QuickLinks",
+  args: quickLinksProps,
+} satisfies Story;
