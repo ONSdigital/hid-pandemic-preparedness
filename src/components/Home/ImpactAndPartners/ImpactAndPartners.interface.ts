7b98baf2653f8996e0ef6a8ae54fb3c26a1e834c
JonnyAdcockAND <jonathan.adcock@ext.ons.gov.uk>
2025-09-14 10:32:13 +0100

    added new interfaces for Partners

--- src/components/Home/ImpactAndPartners/ImpactAndPartners.interface.ts
+++ src/components/Home/ImpactAndPartners/ImpactAndPartners.interface.ts
@@ -1,11 +1,21 @@
+import type { LinkData } from "../../../types/LinkData";
+
 export interface ImpactData {
+  id: string;
   icon: "countries" | "experts" | "users";
   title: string;
   subTitle: string;
 }
 
+export interface PartnerData {
+  id: string;
+  imgSrcPath: string;
+  link: LinkData;
+}
+
 export interface ImpactAndPartnersProps {
   impactTitle: string;
-  partnersTitle: string;
   impactItems: ImpactData[];
+  partnersTitle: string;
+  partnerItems: PartnerData[];
 }
