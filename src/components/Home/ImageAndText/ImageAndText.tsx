26d49a95c43dd029894e96b2ded68a0a75648c08
JonnyAdcockAND <jonathan.adcock@ext.ons.gov.uk>
2025-09-15 10:53:57 +0100

    updated with image

--- src/components/Home/ImageAndText/ImageAndText.tsx
+++ src/components/Home/ImageAndText/ImageAndText.tsx
@@ -7,27 +7,19 @@ import styles from "./ImageAndText.module.scss";
 
 export const ImageAndText: FC<ImageAndTextProps> = (props) => {
   return (
-    <div className={clsx("w-100", styles["impact-and-text-bg"])}>
-      <div
-        className={clsx(
-          "container-lg",
-          styles["impact-and-text-container"],
-          "py-4",
-          "text-primary",
-        )}
-      >
-        <div className={clsx("row", "justify-content-end", "py-2")}>
-          <div className={clsx("col-md-8")}>
-            <h3 className={clsx("heading-m")}>{props.title}</h3>
+    <div className={clsx("w-100", styles["image-and-text-bg"])}>
+      <div className={clsx("container-lg", "py-4", "text-primary")}>
+        <div className={clsx("row", "py-2")}>
+          <div className={clsx("col-md-4", styles["image-and-text-img"])}>
+            <img
+              src={props.image.srcPath}
+              alt={props.image.altText}
+              className={clsx("img-fluid")}
+            />
           </div>
-        </div>
-        <div className={clsx("row", "justify-content-end", "py-2")}>
           <div className={clsx("col-md-8")}>
-            <p>{props.subTitle}</p>
-          </div>
-        </div>
-        <div className={clsx("row", "justify-content-end", "py-2")}>
-          <div className={clsx("col-md-8")}>
+            <h3 className={clsx("heading-m", "py-2")}>{props.title}</h3>
+            <p className={clsx("py-2")}>{props.subTitle}</p>
             <Link asButton={true} buttonVariant="secondary" {...props.link} />
           </div>
         </div>
