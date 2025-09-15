ddb6bb404f84e08f1afc4d7b7e5244c05dc6b2cf
JonnyAdcockAND <jonathan.adcock@ext.ons.gov.uk>
2025-09-15 11:29:39 +0100

    updating config

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,8 +1,19 @@
 import type { Preview } from "@storybook/react-vite";
+import { MINIMAL_VIEWPORTS } from "storybook/viewport";
 
 import "../src/styles/index.scss";
 import "../src/styles/bootstrap-5.3.8/dist/js/bootstrap.bundle";
 import "@fontsource-variable/open-sans";
+
+const customViewports = {
+  desktop: {
+    name: "Desktop 1440px",
+    styles: {
+      width: "1440px",
+    },
+    type: "desktop",
+  },
+};
 
 const preview: Preview = {
   parameters: {
@@ -10,6 +21,12 @@ const preview: Preview = {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
+      },
+      viewport: {
+        options: {
+          ...MINIMAL_VIEWPORTS,
+          ...customViewports,
+        },
       },
     },
   },
