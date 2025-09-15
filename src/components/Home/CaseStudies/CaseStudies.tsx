f97f05e1c17df1ee95655d7af0e691a3c343b703
Jake Andrews <jake.andrews@ext.ons.gov.uk>
2025-09-15 17:35:29 +0100

    Case studies organism to use container-lg wrapping div

--- src/components/Home/CaseStudies/CaseStudies.tsx
+++ src/components/Home/CaseStudies/CaseStudies.tsx
@@ -4,22 +4,25 @@ import clsx from "clsx";
 import type { CaseStudiesProps } from "./CaseStudies.interface";
 import { CardCaseStudy } from "../../CardCaseStudy/CardCaseStudy";
 import { CardCaseStudySmall } from "../../CardCaseStudySmall/CardCaseStudySmall";
+
 import styles from "./CaseStudies.module.scss";
 
 export const CaseStudies: FC<CaseStudiesProps> = (props) => {
   return (
-    <div className={clsx(styles["case-studies__container"])}>
-      <div className={clsx("mb-5", "row")}>
-        <h4 className={clsx("heading-l")}>{props.title}</h4>
-      </div>
-      <div className={clsx("row", "row-cols-1", "row-cols-lg-2")}>
-        <div className={clsx("col")}>
-          <CardCaseStudy {...props.mainCard} />
+    <div className={clsx("w-100", styles["case-studies__container"])}>
+      <div className="container-lg">
+        <div className={clsx("mb-5", "row")}>
+          <h4 className={clsx("heading-l")}>{props.title}</h4>
         </div>
-        <div className={clsx("col", "d-flex", "flex-column", "gap-5")}>
-          {props.smallCards.map((card) => (
-            <CardCaseStudySmall key={card.id} {...card} />
-          ))}
+        <div className={clsx("row", "row-cols-1", "row-cols-lg-2")}>
+          <div className={clsx("col", "mb-5", "mb-lg-0")}>
+            <CardCaseStudy {...props.mainCard} />
+          </div>
+          <div className={clsx("col", "d-flex", "flex-column", "gap-5")}>
+            {props.smallCards.map((card) => (
+              <CardCaseStudySmall key={card.id} {...card} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
