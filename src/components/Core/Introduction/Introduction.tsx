16ed1cebe797842bb31412ec4c55dfc098c0dd77
JonnyAdcockAND <jonathan.adcock@ext.ons.gov.uk>
2025-09-16 12:22:47 +0100

    updated styles

--- src/components/Core/Introduction/Introduction.tsx
+++ src/components/Core/Introduction/Introduction.tsx
@@ -8,7 +8,15 @@ export const Introduction: FC<IntroductionProps> = (props) => {
   const jumpToString: string = "Jump to";
 
   return (
-    <div className={clsx("container")}>
+    <div
+      className={clsx(
+        "container",
+        "py-4",
+        "border",
+        "rounded",
+        styles["border-color"],
+      )}
+    >
       <div className={clsx("row")}>
         <h3 className={clsx("heading-m")}>{props.title}</h3>
       </div>
@@ -20,7 +28,7 @@ export const Introduction: FC<IntroductionProps> = (props) => {
         <h4 className={clsx("heading-s")}>{`${jumpToString}:`}</h4>
       </div>
       <div className={clsx("row")}>
-        <ul>
+        <ul className={clsx("ps-5")}>
           {props.chapters.map((chapter) => (
             <li key={chapter.id}>
               <a href={chapter.href}>{chapter.label}</a>
@@ -31,18 +39,3 @@ export const Introduction: FC<IntroductionProps> = (props) => {
     </div>
   );
 };
-
-// <div className={clsx("w-100", styles["header-bg"])}>
-//   <div className={clsx("container-lg", "py-4", "text-light")}>
-//     <div className={clsx("row")}>
-//       <div className={clsx("col")}>
-//         <Breadcrumb {...props.breadcrumbs} />
-//       </div>
-//     </div>
-//     <div className={clsx("row")}>
-//       <div className={clsx("col")}>
-//         <h1 className={clsx("heading-l")}>{props.title}</h1>
-//       </div>
-//     </div>
-//   </div>
-// </div>
