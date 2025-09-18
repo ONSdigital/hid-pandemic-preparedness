fffa9cb1f3896bf563c9097f36989974be8b907b
Jake Andrews <jake.andrews@ext.ons.gov.uk>
2025-09-18 14:35:50 +0100

    Markdown files for LearningResourceBLock

--- src/components/Core/LearningResourceBlock/LearningResourceBlock.stories.tsx
+++ src/components/Core/LearningResourceBlock/LearningResourceBlock.stories.tsx
@@ -1,5 +1,9 @@
 import type { Meta, StoryObj } from "@storybook/react";
+import DOMPurify from "dompurify";
+import { v4 as uuidv4 } from "uuid";
 
+// import learningResourceMd1 from "../../../content/LearningResource/introduction/sections/1.md?raw";
+// import learningResourceMd2 from "../../../content/LearningResource/introduction/sections/2.md?raw";
 import { LearningResourceBlock } from "./LearningResourceBlock";
 
 const meta = {
