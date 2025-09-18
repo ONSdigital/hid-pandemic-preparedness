14a9b5c99973022e550c66ea459f316772f56dbf
Jake Andrews <jake.andrews@ext.ons.gov.uk>
2025-09-18 11:07:46 +0100

    Move LearningModule organism into /core

---
+++ src/components/Core/LearningModule/LearningModule.interface.ts
@@ -0,0 +1,7 @@
+import type { LearningModuleCardProps } from "../LearningModuleCard/LearningModuleCard.interface";
+import type { LearningModuleNavProps } from "../LearningModuleNav/LearningModuleNav.interface";
+
+export interface LearningModuleProps {
+  cardProps: LearningModuleCardProps;
+  navProps: LearningModuleNavProps;
+}
---
+++ src/components/Core/LearningModule/LearningModule.stories.tsx
@@ -0,0 +1,43 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import DOMPurify from "dompurify";
+
+import chaptersData from "../../content/chapters.json";
+import learningModuleCardData from "../../content/learningModuleCard.json";
+import markdownContent from "../../content/learning-module-content.md?raw";
+import { LearningModule } from "./LearningModule";
+import type { LearningModuleProps } from "./LearningModule.interface";
+import { parseMarkdown } from "../../helpers/parseMarkdown";
+import type { TagData } from "../../types/TagData";
+
+const meta = {
+  argTypes: {},
+  component: LearningModule,
+  parameters: {
+    layout: "centered",
+  },
+  title: "Organisms/LearningModule",
+} satisfies Meta<typeof LearningModule>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// Use helper to parse markdown to html
+const htmlContent = await parseMarkdown(markdownContent);
+
+const learningModuleProps: LearningModuleProps = {
+  cardProps: {
+    // Sanitizing using dompurify here as this is running client side
+    htmlContent: DOMPurify.sanitize(htmlContent),
+    ...learningModuleCardData,
+    tags: learningModuleCardData.tags as TagData[],
+  },
+  navProps: {
+    chapters: chaptersData,
+    activeId: "chapter1",
+  },
+};
+
+export const LearningModuleStory = {
+  args: learningModuleProps,
+  name: "LearningModule",
+} satisfies Story;
