35435d26d6bbd0a60e43e8047268db41d0172fd3
Jake Andrews <jake.andrews@ext.ons.gov.uk>
2025-09-19 20:20:17 +0100

    Update QuestionBlock scss class

--- src/components/QuestionBank/QuestionBlock/QuestionBlock.tsx
+++ src/components/QuestionBank/QuestionBlock/QuestionBlock.tsx
@@ -8,7 +8,14 @@ import styles from "./QuestionBlock.module.scss";
 
 export const QuestionBlock: FC<QuestionBlockProps> = (props) => {
   return (
-    <div className={clsx("p-4", "border", "rounded", styles["border-color"])}>
+    <div
+      className={clsx(
+        "p-4",
+        "border",
+        "rounded",
+        styles["question-block__container"],
+      )}
+    >
       <div className={clsx("d-inline-flex", "pt-4", "pb-2")}>
         {props.tags.map((tag) => (
           <Tag {...tag} />
