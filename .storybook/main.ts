d3daf7bf38664bccddf56fad83183372aa900b7b
JonnyAdcockAND <jonathan.adcock@ext.ons.gov.uk>
2025-09-21 11:51:42 +0100

    silencing scss deprecation warnings outputted by bootstrap

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -13,6 +13,21 @@ const config: StorybookConfig = {
   async viteFinal(config) {
     return mergeConfig(config, {
       plugins: [viteTsconfig()],
+      css: {
+        preprocessorOptions: {
+          scss: {
+            // need this until bootstrap is updated, see https://github.com/twbs/bootstrap/issues/40962
+            quietDeps: true,
+            silenceDeprecations: [
+              "mixed-decls",
+              "import",
+              "color-functions",
+              "global-builtin",
+            ],
+            verbose: false,
+          },
+        },
+      },
     });
   },
 };
--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,21 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   vite: {
     plugins: [tsconfigPaths()],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          // need this until bootstrap is updated, see https://github.com/twbs/bootstrap/issues/40962
+          quietDeps: true,
+          silenceDeprecations: [
+            "mixed-decls",
+            "import",
+            "color-functions",
+            "global-builtin",
+          ],
+          verbose: false,
+        },
+      },
+    },
   },
   integrations: [react()],
 });
