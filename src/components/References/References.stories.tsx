21dfb462fb1766c8ba2b407e8b78edffd718c17f
JonnyAdcockAND <jonathan.adcock@ext.ons.gov.uk>
2025-09-22 21:21:04 +0100

    updated references so input references array is optional and updated pages that didn't require references to just be empty

--- src/components/References/References.stories.tsx
+++ src/components/References/References.stories.tsx
@@ -1,7 +1,21 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
-import referenceData from "../../content/Home/references.json";
+import referenceData from "@content/Home/references.json";
+
 import { References } from "./References";
+import type { ReferencesProps } from "./References.interface";
+
+// Defines some options for button children so we can show how different button content is rendered
+interface ReferencesOptions {
+  None: ReferencesProps;
+  References: ReferencesProps;
+}
+
+// Define refences options
+const referencesOptions: ReferencesOptions = {
+  None: { references: undefined },
+  References: { references: referenceData.references },
+};
 
 const meta = {
   component: References,
@@ -23,5 +37,5 @@ type Story = StoryObj<typeof meta>;
 
 export const ReferencesStory = {
   name: "References",
-  args: referenceData,
+  args: referencesOptions.None,
 } satisfies Story;
