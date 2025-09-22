825592a4132cc9711b0d7e76f5d4c93bcc6789f1
Jake Andrews <jake.andrews@ext.ons.gov.uk>
2025-09-22 18:41:26 +0100

    Quick links update for bug sprint

--- src/components/Home/QuickLinks/QuickLinks.tsx
+++ src/components/Home/QuickLinks/QuickLinks.tsx
@@ -1,38 +1,44 @@
 import clsx from "clsx";
 import type { FC } from "react";
 
-import { Link } from "../../Link/Link";
+import { Link } from "@components/Link/Link";
 import type { QuickLinksProps } from "./QuickLinks.interface";
 
+import styles from "./QuickLinks.module.scss";
+
 export const QuickLinks: FC<QuickLinksProps> = (props) => {
+  const itemsPerList = 2;
   return (
-    <div className={clsx("w-100", "bg-primary")}>
+    <div className={clsx("w-100", styles["quick-links-bg"])}>
       <div className={clsx("container-lg", "text-light", "py-4")}>
         <div className={clsx("row")}>
-          <h4 className={clsx("heading-s")}>{props.title}</h4>
+          <h4 className={clsx("heading-s", "pb-4")}>{props.title}</h4>
         </div>
-        <div className={clsx("row", "row-cols-1", "row-cols-lg-5")}>
+        <div className={clsx("row")}>
           {props.navItems.map((navItem) => (
             <div
-              className={clsx("col", "py-3", "d-flex", "flex-column")}
+              className={clsx("col-12", "col-md", "d-flex", "flex-column")}
               key={navItem.id}
             >
-              <p className={clsx("fw-bold")}>{navItem.label}</p>
+              <p className={clsx("body-regular", "fw-bold")}>{navItem.label}</p>
               <div
                 className={clsx(
-                  "list-group",
-                  "list-group-flush",
                   "d-none",
                   "d-md-block",
+                  "list-group",
+                  "list-group-flush",
+                  "py-4",
                 )}
               >
-                {navItem.children?.map((link) => (
+                {navItem.children?.slice(0, itemsPerList).map((link) => (
                   <a
                     href={link.href}
                     className={clsx(
+                      "body-regular",
                       "text-light",
                       "list-group-item",
                       "list-group-item-action",
+                      "py-2",
                       link.disabled && "disabled",
                     )}
                     key={link.id}
@@ -41,12 +47,9 @@ export const QuickLinks: FC<QuickLinksProps> = (props) => {
                   </a>
                 ))}
               </div>
-              <Link
-                href={"/"}
-                label={"View all"}
-                textInverse={true}
-                className="mt-auto"
-              />
+              <div className={clsx("mt-2", "mb-3", "mt-auto")}>
+                <Link href={"/"} label={"View all"} textInverse={true} />
+              </div>
             </div>
           ))}
         </div>
