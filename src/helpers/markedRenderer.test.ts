26dff534e116fa6da0fcd4c8405a8d1933a4b301
JonnyAdcockAND <jonathan.adcock@ext.ons.gov.uk>
2025-09-23 14:28:31 +0100

    added custom renderer

---
+++ src/helpers/markedRenderer.test.ts
@@ -0,0 +1,43 @@
+import { marked } from "marked";
+import util from "util";
+import { describe, it, expect } from "vitest";
+
+import { markedRenderer } from "./markedRenderer";
+marked.use({ renderer: markedRenderer });
+
+describe("markedRenderer", () => {
+  const tableMarkdown: string =
+    "| Name  | Age | City     |\n" +
+    "|-------|-----|----------|\n" +
+    "| Alice | 30  | New York |\n" +
+    "| Bob   | 25  | London   |";
+
+  const expectedOutput: string =
+    '<table class="table">\n' +
+    "<thead>\n" +
+    "<tr>\n" +
+    "<th>Name</th>\n" +
+    "<th>Age</th>\n" +
+    "<th>City</th>\n" +
+    "</tr>\n" +
+    "</thead>\n" +
+    "<tbody><tr>\n" +
+    "<td>Alice</td>\n" +
+    "<td>30</td>\n" +
+    "<td>New York</td>\n" +
+    "</tr>\n" +
+    "<tr>\n" +
+    "<td>Bob</td>\n" +
+    "<td>25</td>\n" +
+    "<td>London</td>\n" +
+    "</tr>\n" +
+    "</tbody></table>\n";
+
+  it("parses table markdown into html correctly", async () => {
+    const result = marked.parse(tableMarkdown);
+
+    console.log(util.inspect(result, { depth: null, colors: true }));
+
+    expect(result).toMatch(expectedOutput);
+  });
+});
---
+++ src/helpers/markedRenderer.ts
@@ -0,0 +1,23 @@
+import { clsx } from "clsx";
+import { marked } from "marked";
+import type { Tokens } from "marked";
+
+export const markedRenderer = new marked.Renderer();
+
+markedRenderer.table = function (token: Tokens.Table) {
+  const tableClasses: string = clsx("table");
+
+  const headerHtml = `<tr>\n${token.header.map((cell) => this.tablecell(cell)).join("")}</tr>\n`;
+  const bodyHtml = token.rows
+    .map(
+      (row) =>
+        `<tr>\n${row.map((cell) => this.tablecell(cell)).join("")}</tr>\n`,
+    )
+    .join("");
+
+  return `<table class="${tableClasses}">
+<thead>
+${headerHtml}</thead>
+<tbody>${bodyHtml}</tbody></table>
+`;
+};
