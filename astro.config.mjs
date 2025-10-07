2c4e10e65038ad639da6936e23c5e1ea07fcb33e
JonnyAdcockAND <jonathan.adcock@ext.ons.gov.uk>
2025-10-07 15:11:16 +0100

    updating the astro config to load different settings based on deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,50 @@
 import node from "@astrojs/node";
 import react from "@astrojs/react";
 import { defineConfig } from "astro/config";
+import { loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+
+const mode = process.env.NODE_ENV;
+const env = loadEnv(mode, process.cwd(), "");
+
+const ASTRO_OUTPUT = env.ASTRO_OUTPUT || "static";
+
+// If output is server use node adapter
+const ASTRO_ADAPTER =
+  ASTRO_OUTPUT == "server"
+    ? node({
+        mode: "middleware",
+      })
+    : undefined;
+
+// If output is server set assets prefix to cdn base url if available
+const ASTRO_ASSETS_PREFIX =
+  ASTRO_OUTPUT == "server" ? env.PREVIEW_CDN_BASE_URL || undefined : undefined;
+
+// If output is server set extra ssr vite config
+const VITE_SSR =
+  ASTRO_OUTPUT == "server"
+    ? {
+        // Need to add these as external dependencies as they could not be bundled as part of SSR build
+        external: [
+          "common-ancestor-path",
+          "cssesc",
+          "extend",
+          "picomatch",
+          "prismjs",
+          "serverless-http",
+        ],
+        noExternal: true,
+      }
+    : undefined;
 
 // https://astro.build/config
 export default defineConfig({
   build: {
-    assetsPrefix: "https://d8sn29szhcb2a.cloudfront.net/client",
+    assetsPrefix: ASTRO_ASSETS_PREFIX,
   },
-  output: "server",
-  adapter: node({
-    mode: "middleware",
-  }),
+  output: ASTRO_OUTPUT,
+  adapter: ASTRO_ADAPTER,
   vite: {
     plugins: [tsconfigPaths()],
     build: {
@@ -33,18 +66,7 @@ export default defineConfig({
         },
       },
     },
-    ssr: {
-      // Need to add these as external dependencies as they could not be bundled as part of SSR build
-      external: [
-        "common-ancestor-path",
-        "cssesc",
-        "extend",
-        "picomatch",
-        "prismjs",
-        "serverless-http",
-      ],
-      noExternal: true,
-    },
+    ssr: VITE_SSR,
   },
   integrations: [react()],
 });
