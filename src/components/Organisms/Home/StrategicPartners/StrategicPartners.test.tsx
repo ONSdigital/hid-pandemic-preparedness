ab8306b2fe4dd51d1c6712e65501c0d2af1069ef
JonnyAdcockAND <jonathan.adcock@ext.ons.gov.uk>
2025-10-10 11:41:59 +0100

    fixing test

--- src/components/Organisms/Home/StrategicPartners/StrategicPartners.test.tsx
+++ src/components/Organisms/Home/StrategicPartners/StrategicPartners.test.tsx
@@ -3,45 +3,19 @@ import { render, screen } from "@testing-library/react";
 import { describe, expect, test } from "vitest";
 import "@testing-library/jest-dom";
 
+import strategicPartnersProps from "./strategic-partners.json";
 import type { StrategicPartnersProps } from "./StrategicPartners.interface";
 import { StrategicPartners } from "./StrategicPartners";
 
 describe("StrategicPartners component", () => {
   const baseProps: StrategicPartnersProps = {
+    ...(strategicPartnersProps as StrategicPartnersProps),
     title: "strategic partners",
-    partners: [
-      {
-        _uid: uuidv4(),
-        link: {
-          id: uuidv4(),
-          cached_url: "https://example.com",
-          fieldtype: "multilink",
-          linktype: "url",
-          url: "https://example.com",
-        },
-        logo: {
-          id: 99913189991725,
-          alt: null,
-          name: "",
-          focus: "",
-          title: "",
-          source: "",
-          filename: null,
-          copyright: "",
-          fieldtype: "asset",
-          meta_data: {
-            alt: "CEMIC logo",
-            title: "",
-            source: "",
-            copyright: "",
-          },
-          is_external_url: false,
-        },
-      },
-    ],
   };
 
   test("renders `strategic-partners-title` as sentence case", () => {
+    // Make sure title is lower case first
+    expect(baseProps.title).toBe("strategic partners");
     render(<StrategicPartners {...baseProps} />);
     const title = screen.getByRole("strategic-partners-title");
     expect(title.textContent).toBe("Strategic partners");
