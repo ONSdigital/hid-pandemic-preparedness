e12fb7d0d7808691062eb91b22d5c015e2ccf0d1
admilne <andrew.milne@ext.ons.gov.uk>
2025-10-13 15:08:24 +0100

    Update vitest config to allow for @components alias

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,14 @@
 /// <reference types="vitest" />
+import path from "path";
 import { defineConfig } from "vitest/config";
 
+
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "src/components"),
+    },
+  },
   test: {
     globals: true,
     environment: "jsdom",
