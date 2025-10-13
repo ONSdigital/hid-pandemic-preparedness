e675bb2394f986c5530073a9b1a551016498a985
Jake Andrews <jake.andrews@ext.ons.gov.uk>
2025-10-13 17:14:51 +0100

    Custom pagination bullets in carousel

--- src/components/Core/Carousel/Carousel.tsx
+++ src/components/Core/Carousel/Carousel.tsx
@@ -10,6 +10,7 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { ArrowButton } from "../../ArrowButton/ArrowButton";
 import clsx from "clsx";
+import styles from "./Carousel.module.scss";
 
 export interface SwiperButtonProps {
   swiperRef: RefObject<SwiperInstance | null>;
@@ -19,121 +20,135 @@ export const Carousel: FC = () => {
   const swiperRef = useRef<SwiperType | null>(null);
   const hiddenNavigation = clsx("d-none", "d-md-block");
   return (
-    <div className="d-flex align-items-center gap-3">
-      <div className={hiddenNavigation}>
-        <ArrowButton
-          ariaLabel="Previous"
-          direction="left"
-          onClick={() => swiperRef.current?.slidePrev()}
-          type="button"
-          variant="secondary"
-        />
-      </div>
+    <>
+      <div className="d-flex align-items-center gap-3">
+        <div className={hiddenNavigation}>
+          <ArrowButton
+            ariaLabel="Previous"
+            direction="left"
+            onClick={() => swiperRef.current?.slidePrev()}
+            type="button"
+            variant="secondary"
+          />
+        </div>
 
-      <Swiper
-        loop={true}
-        modules={[Navigation, Pagination]}
-        onSwiper={(swiper: any) => (swiperRef.current = swiper)}
-        pagination={true}
-        spaceBetween={50}
-        slidesPerView={3}
-        breakpoints={{
-          0: {
-            slidesPerView: 1,
-          },
-          768: {
-            slidesPerView: 3,
-          },
-        }}
-      >
-        <SwiperSlide>
-          <div
-            style={{
-              backgroundColor: "#f99",
-              height: "200px",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            Slide 1
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div
-            style={{
-              backgroundColor: "#9f9",
-              height: "200px",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            Slide 2
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div
-            style={{
-              backgroundColor: "#99f",
-              height: "200px",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            Slide 3
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div
-            style={{
-              backgroundColor: "#ffcc00",
-              height: "200px",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            Slide 4
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div
-            style={{
-              backgroundColor: "#00cccc",
-              height: "200px",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            Slide 5
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div
-            style={{
-              backgroundColor: "#cc00cc",
-              height: "200px",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            Slide 6
-          </div>
-        </SwiperSlide>
-      </Swiper>
-      <div className={hiddenNavigation}>
-        <ArrowButton
-          ariaLabel="Next"
-          direction="right"
-          onClick={() => swiperRef.current?.slideNext()}
-          type="button"
-          variant="secondary"
-        />
+        <Swiper
+          loop={true}
+          modules={[Navigation, Pagination]}
+          onSwiper={(swiper: any) => (swiperRef.current = swiper)}
+          pagination={{
+            clickable: true,
+            el: "#container-for-pagination-bullets",
+            type: "bullets",
+            bulletClass: styles["swiper-default-bullets"],
+            bulletActiveClass: styles["swiper-active-bullets"],
+          }}
+          spaceBetween={50}
+          slidesPerView={3}
+          breakpoints={{
+            0: {
+              slidesPerView: 1,
+            },
+            // md
+            768: {
+              slidesPerView: 3,
+            },
+          }}
+        >
+          <SwiperSlide>
+            <div
+              style={{
+                backgroundColor: "#f99",
+                height: "200px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              Slide 1
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div
+              style={{
+                backgroundColor: "#9f9",
+                height: "200px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              Slide 2
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div
+              style={{
+                backgroundColor: "#99f",
+                height: "200px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              Slide 3
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div
+              style={{
+                backgroundColor: "#ffcc00",
+                height: "200px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              Slide 4
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div
+              style={{
+                backgroundColor: "#00cccc",
+                height: "200px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              Slide 5
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div
+              style={{
+                backgroundColor: "#cc00cc",
+                height: "200px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              Slide 6
+            </div>
+          </SwiperSlide>
+        </Swiper>
+        <div className={hiddenNavigation}>
+          <ArrowButton
+            ariaLabel="Next"
+            direction="right"
+            onClick={() => swiperRef.current?.slideNext()}
+            type="button"
+            variant="secondary"
+          />
+        </div>
       </div>
-    </div>
+      {/* Swiper.js references this div when creating custom pagination bullets*/}
+      <div
+        id="container-for-pagination-bullets"
+        className="d-flex d-md-none justify-content-center gap-3 mt-4"
+      ></div>
+    </>
   );
 };
