3fb7b2a886960b923fc8aab7427a53ddc3d030c4
JonnyAdcockAND <jonathan.adcock@ext.ons.gov.uk>
2025-10-15 12:50:57 +0100

    moved breadcrumb

--- src/components/Molecules/Breadcrumb/Breadcrumb.interface.ts
+++
@@ -1,5 +0,0 @@
-import type { LinkData } from "../../../types/LinkData";
-
-export interface BreadcrumbProps {
-  items: LinkData[];
-}
---
+++ src/components/Molecules/Core/Breadcrumb/Breadcrumb.interface.ts
@@ -0,0 +1,5 @@
+import type { Link } from "@src/types/Link";
+
+export interface BreadcrumbProps {
+  items: Link[];
+}
