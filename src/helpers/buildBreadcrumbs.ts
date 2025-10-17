abd7946236bf62f13702197d0ad15e417b535b1e
JonnyAdcockAND <jonathan.adcock@ext.ons.gov.uk>
2025-10-17 17:06:00 +0100

    updated to build breadcrumbs and use dynamic routing for SSG and SSR

---
+++ src/helpers/buildBreadcrumbs.ts
@@ -0,0 +1,55 @@
+import type { BreadcrumbProps } from "@src/components/Molecules/Core/Breadcrumb/Breadcrumb.interface";
+
+import { fetchStories } from "@src/helpers/fetchContent";
+
+// Takes an input `fullSlug` and builds an output `BreadcrumbProps` object consisting of the
+// parent pages up to the homepage
+export async function buildBreadcrumbs(
+  fullSlug: string,
+): Promise<BreadcrumbProps> {
+  let stories = null;
+  let returnBreadcrumbs: BreadcrumbProps = {
+    items: [],
+  };
+
+  let bySlugsItems: string[] = ["home"];
+
+  // Build the `by_slugs` comma separated match string using the input `fullSlug`
+  const slugItems: string[] = fullSlug.split("/");
+
+  if (slugItems.length > 1) {
+    // We create a search slug for each part of the slug
+    let currentPath = "";
+
+    for (const segment of slugItems) {
+      // Only process valid non-empty strings
+      if (segment !== "") {
+        currentPath = currentPath ? `${currentPath}/${segment}` : segment;
+        bySlugsItems.push(currentPath + "/");
+      }
+    }
+  }
+  // Sorting by id will ensure home returns first and most distant child last
+  const { data } = await fetchStories({
+    by_slugs: bySlugsItems.join(","),
+    sort_by: "id:asc",
+  });
+  stories = data?.stories;
+
+  if (stories) {
+    // Loop through the returned stories and build the breadcrumbs
+    returnBreadcrumbs = {
+      items: stories.map(({ uuid, name, full_slug, path }) => ({
+        fieldtype: "multilink",
+        linktype: "",
+        id: uuid,
+        // Use path if defined as link otherwise full slug prepended with /
+        url: path ? path : `/${full_slug}`,
+        cached_url: path ? path : `/${full_slug}`,
+        title: name,
+      })),
+    };
+  }
+
+  return returnBreadcrumbs;
+}
