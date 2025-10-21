ec4f39f48c98096d327a5fc035a308fff09b55b2
admilne <andrew.milne@ext.ons.gov.uk>
2025-10-21 16:28:30 +0100

    Update home page hero image

--- src/components/Organisms/Home/Header/Header.tsx
+++ src/components/Organisms/Home/Header/Header.tsx
@@ -11,27 +11,26 @@ import styles from "./Header.module.scss";
 export const Header: FC<HeaderProps> = (props) => {
   return (
     <div className={clsx("w-100", styles["header-bg"])}>
-      <div className={clsx("container-lg", "py-4", "text-light")}>
+      <div className={clsx("container-lg", "text-light")}>
         <div className={clsx("row")}>
-          <div className={clsx("col-md-6")}>
+          <div className={clsx("col-lg-6", "py-4", "position-relative", "z-1")}>
             <Breadcrumb {...props.breadcrumbs} />
+
+            <h1 className={clsx("heading-xl")}>{sentenceCase(props.title)}</h1>
+            <p className={clsx("py-lg-4")}>{props.subTitle}</p>
+
+            <div className={clsx("col-xl-9", "pt-2")}>
+              <SearchBar placeholder={props.searchPlaceholderText} />
+            </div>
           </div>
-        </div>
-        <div className={clsx("row")}>
-          <div className={clsx("col-md-6")}>
-            <h1 role={"header-title"} className={clsx("heading-xl")}>
-              {sentenceCase(props.title)}
-            </h1>
-          </div>
-        </div>
-        <div className={clsx("row", "py-lg-4")}>
-          <div className={clsx("col-md-6")}>
-            <p>{props.subTitle}</p>
-          </div>
-        </div>
-        <div className={clsx("row", "py-2")}>
-          <div className={clsx("col-md-5")}>
-            <SearchBar placeholder={props.searchPlaceholderText} />
+          <div className={clsx("col-lg-6")}>
+            <div className={clsx(styles["header-image-wrapper"])}>
+              <img
+                src="/images/home-header-world-of-data.jpg"
+                alt=""
+                className={clsx(styles["header-image"])}
+              />
+            </div>
           </div>
         </div>
       </div>
