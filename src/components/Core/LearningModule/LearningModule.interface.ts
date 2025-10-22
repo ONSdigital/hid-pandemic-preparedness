304ee62e6deef5025464567d4cebe5f5896527eb
JonnyAdcockAND <jonathan.adcock@ext.ons.gov.uk>
2025-10-22 12:50:37 +0100

    removing unused files

--- src/components/Core/LearningModule/LearningModule.interface.ts
+++
@@ -1,7 +0,0 @@
-import type { LearningModuleCardProps } from "@components/LearningModuleCard/LearningModuleCard.interface";
-import type { LearningModuleNavProps } from "@components/LearningModuleNav/LearningModuleNav.interface";
-
-export interface LearningModuleProps {
-  cardProps: LearningModuleCardProps;
-  navProps: LearningModuleNavProps;
-}
