ffe525d088dbcb0e8ea1f1bbf1d8ff6a2f4d6ff1
JonnyAdcockAND <jonathan.adcock@ext.ons.gov.uk>
2025-10-28 10:40:27 +0000

    fixed linting and typescript errors

--- src/components/Organisms/Unit/UnitNav/UnitNav.interface.ts
+++ src/components/Organisms/Unit/UnitNav/UnitNav.interface.ts
@@ -2,8 +2,8 @@ import type { Chapter } from "@src/types/Chapter";
 import type { Link } from "@src/types/Link";
 
 export interface UnitNavProps {
-  githubLink: Link;
   chapters: Chapter[];
+  githubLink?: Link;
   activeChapterId?: string;
   onSelect?: (selectedId: string) => void; // eslint-disable-line no-unused-vars
 }
