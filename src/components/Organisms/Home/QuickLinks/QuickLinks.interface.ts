f77b0ec99178e5b2a6b3fabf150bc58f4c729f3f
admilne <andrew.milne@ext.ons.gov.uk>
2025-10-30 14:24:39 +0000

    Update QuickLinksItem interface to better reflect it's contents

--- src/components/Organisms/Home/QuickLinks/QuickLinks.interface.ts
+++ src/components/Organisms/Home/QuickLinks/QuickLinks.interface.ts
@@ -4,6 +4,9 @@ import * as RemixIcons from "@remixicon/react";
 interface QuickLinksItem {
   icon: keyof typeof RemixIcons;
   link: Link;
+  _uid: string;
+  type: string;
+  component: string;
 }
 
 export interface QuickLinksProps {
