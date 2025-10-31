c0cc82497b434acccc6c68abdc3e048d6cb804df
Jake Andrews <jake.andrews@ext.ons.gov.uk>
2025-10-31 14:40:31 +0000

    WIP NavBar interface and storybook based on CMS data structures

--- src/components/Organisms/Core/NavBar/NavBar.interface.ts
+++ src/components/Organisms/Core/NavBar/NavBar.interface.ts
@@ -1,5 +1,27 @@
 import type { Asset } from "@localTypes/Asset";
+import type { Link } from "@/src/types/Link";
+import type { StoryblokRichTextNode } from "@storyblok/astro";
+
+interface NavColumn {
+  _uid: string;
+  title: string;
+  NavLink: NavLink[];
+}
+
+interface NavLink {
+  _uid: string;
+  link: Link;
+}
+
+interface NavItem {
+  _uid: string;
+  label: string;
+  overview: StoryblokRichTextNode;
+  NavColumn: NavColumn[];
+}
 
 export interface NavBarProps {
   logo: Asset;
+  navItems: NavItem[];
+  navLinks: NavLink[];
 }
--- src/components/Organisms/Core/NavBar/NavBar.tsx
+++ src/components/Organisms/Core/NavBar/NavBar.tsx
@@ -2,7 +2,36 @@ import type { FC } from "react";
 
 import type { NavBarProps } from "./NavBar.interface";
 import { Image } from "@components/Molecules/Core/Image/Image";
+import { Link } from "@components/Molecules/Core/Link/Link";
+import { SearchBar } from "@components/Molecules/SearchBar/SearchBar";
 
 export const NavBar: FC<NavBarProps> = (props) => {
-  return <Image {...props.logo} />;
+  const hasNavItems = props.navItems && props.navItems.length > 0;
+  const hasNavLinks = props.navLinks && props.navLinks.length > 0;
+  return (
+    <>
+      {/* Logo */}
+      <Image {...props.logo} />
+
+      {/* Nav Items */}
+      {hasNavItems &&
+        props.navItems.map((navItem) => (
+          <div key={navItem._uid}>
+            {/* Nav Item label */}
+            <h1> {navItem.label}</h1>
+            {/* Nav Item Overview */}
+            {/* {navItem.overview && <TextModule richText={navItem.overview} />} */}
+          </div>
+        ))}
+
+      {/* Nav Links */}
+      {hasNavLinks &&
+        props.navLinks.map((navLink) => (
+          <Link key={navLink._uid} {...navLink.link} />
+        ))}
+
+      {/* SearchBar */}
+      <SearchBar placeholder="TODO searchBar" />
+    </>
+  );
 };
