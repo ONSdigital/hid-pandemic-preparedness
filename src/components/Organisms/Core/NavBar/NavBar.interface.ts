d621c450f27503155fc2134a54e7f00d529427f2
Jake Andrews <jake.andrews@ext.ons.gov.uk>
2025-10-31 14:43:22 +0000

    Reorder interface NavBar

--- src/components/Organisms/Core/NavBar/NavBar.interface.ts
+++ src/components/Organisms/Core/NavBar/NavBar.interface.ts
@@ -2,10 +2,10 @@ import type { Asset } from "@localTypes/Asset";
 import type { Link } from "@/src/types/Link";
 import type { StoryblokRichTextNode } from "@storyblok/astro";
 
-interface NavColumn {
-  _uid: string;
-  title: string;
-  NavLink: NavLink[];
+export interface NavBarProps {
+  logo: Asset;
+  navItems: NavItem[];
+  navLinks: NavLink[];
 }
 
 interface NavLink {
@@ -17,11 +17,11 @@ interface NavItem {
   _uid: string;
   label: string;
   overview: StoryblokRichTextNode;
-  NavColumn: NavColumn[];
+  NavColumn: NavColumn[]; //TODO potentially change this to 'NavMenuColumn'
 }
 
-export interface NavBarProps {
-  logo: Asset;
-  navItems: NavItem[];
-  navLinks: NavLink[];
+interface NavColumn {
+  _uid: string;
+  title: string;
+  NavLink: NavLink[];
 }
