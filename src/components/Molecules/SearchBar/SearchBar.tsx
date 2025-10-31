6cad35aba924371afdd38870d4979d45fd437f8f
Jake Andrews <jake.andrews@ext.ons.gov.uk>
2025-10-31 15:09:41 +0000

    NavBar contents displayed as flex

--- src/components/Molecules/SearchBar/SearchBar.tsx
+++ src/components/Molecules/SearchBar/SearchBar.tsx
@@ -39,7 +39,7 @@ export const SearchBar: FC<SearchBarProps> = (props) => {
 
   return (
     <form role="search" className={clsx("text-dark", styles["input-bg"])}>
-      <div ref={searchContainerRef} className={clsx("input-group", "mb-3")}>
+      <div ref={searchContainerRef} className={clsx("input-group")}>
         <input
           aria-describedby="search-button"
           aria-label={props.placeholder}
--- src/components/Organisms/Core/NavBar/NavBar.tsx
+++ src/components/Organisms/Core/NavBar/NavBar.tsx
@@ -1,6 +1,9 @@
 import type { FC } from "react";
+import clsx from "clsx";
 
+import styles from "./NavBar.module.scss";
 import type { NavBarProps } from "./NavBar.interface";
+
 import { Image } from "@components/Molecules/Core/Image/Image";
 import { Link } from "@components/Molecules/Core/Link/Link";
 import { SearchBar } from "@components/Molecules/SearchBar/SearchBar";
@@ -9,29 +12,40 @@ export const NavBar: FC<NavBarProps> = (props) => {
   const hasNavItems = props.navItems && props.navItems.length > 0;
   const hasNavLinks = props.navLinks && props.navLinks.length > 0;
   return (
-    <>
-      {/* Logo */}
-      <Image {...props.logo} />
+    <nav className={clsx("w-100", styles["navbar-bg"])}>
+      <div className={clsx("container-lg")}>
+        <div
+          className={clsx(
+            "d-flex",
+            "align-items-center",
+            "justify-content-between",
+            "gap-4",
+          )}
+        >
+          {/* Logo */}
+          <Image {...props.logo} />
 
-      {/* Nav Items */}
-      {hasNavItems &&
-        props.navItems.map((navItem) => (
-          <div key={navItem._uid}>
-            {/* Nav Item label */}
-            <h1> {navItem.label}</h1>
-            {/* Nav Item Overview */}
-            {/* {navItem.overview && <TextModule richText={navItem.overview} />} */}
-          </div>
-        ))}
+          {/* Nav Items */}
+          {hasNavItems &&
+            props.navItems.map((navItem) => (
+              <div key={navItem._uid}>
+                {/* Nav Item label */}
+                <h1> {navItem.label}</h1>
+                {/* Nav Item Overview */}
+                {/* {navItem.overview && <TextModule richText={navItem.overview} />} */}
+              </div>
+            ))}
 
-      {/* Nav Links */}
-      {hasNavLinks &&
-        props.navLinks.map((navLink) => (
-          <Link key={navLink._uid} {...navLink.link} />
-        ))}
+          {/* Nav Links */}
+          {hasNavLinks &&
+            props.navLinks.map((navLink) => (
+              <Link key={navLink._uid} {...navLink.link} textInverse={true} />
+            ))}
 
-      {/* SearchBar */}
-      <SearchBar placeholder="TODO searchBar" />
-    </>
+          {/* SearchBar */}
+          <SearchBar placeholder="TODO searchBar" />
+        </div>
+      </div>
+    </nav>
   );
 };
