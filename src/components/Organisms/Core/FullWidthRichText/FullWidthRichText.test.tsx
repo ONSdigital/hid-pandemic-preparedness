f448d57cf93318bd79446c5be0ab98a359df6bea
JonnyAdcockAND <jonathan.adcock@ext.ons.gov.uk>
2025-11-03 13:37:52 +0000

    updated component to use renderRichText in TextModule so we only apply config once in the codebase

--- src/components/Organisms/Core/FullWidthRichText/FullWidthRichText.test.tsx
+++ src/components/Organisms/Core/FullWidthRichText/FullWidthRichText.test.tsx
@@ -2,13 +2,14 @@ import { render } from "@testing-library/react";
 import { describe, expect, test } from "vitest";
 import "@testing-library/jest-dom";
 
-import fullWidthRichTextProps from "./full-width-rich-text.json";
+// Importing raw here to avoid typescript errors when parsing strings to enums
+import fullWidthRichTextProps from "./full-width-rich-text.json?raw";
 import type { FullWidthRichTextProps } from "./FullWidthRichText.interface";
 import { FullWidthRichText } from "./FullWidthRichText";
 
 describe("FullWidthRichText component", () => {
   const baseProps: FullWidthRichTextProps = {
-    ...fullWidthRichTextProps,
+    ...JSON.parse(fullWidthRichTextProps),
   };
 
   test("renders component without error", () => {
