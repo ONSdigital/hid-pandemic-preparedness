d85583d57c28c89e4bde0b271391189d0ba5cf1f
Jake Andrews <jake.andrews@ext.ons.gov.uk>
2025-11-05 17:31:29 +0000

    Mobile Nav content toggles with button

--- src/components/Organisms/Core/NavBar/MobileNav.tsx
+++ src/components/Organisms/Core/NavBar/MobileNav.tsx
@@ -1,10 +1,11 @@
-import type { FC } from "react";
+import { useState, type FC } from "react";
 
 import { Image } from "@components/Molecules/Core/Image/Image";
 
 import type { MobileNavProps } from "./NavBar.interface";
 import { MegaMenu } from "@components/MegaMenu/MegaMenu";
 import { Accordion } from "@components/Molecules/Core/Accordion/Accordion";
+import { SearchBar } from "@components/Molecules/SearchBar/SearchBar";
 
 export const MobileNav: FC<MobileNavProps> = (props) => {
   const hasExpandableItems =
@@ -19,14 +20,34 @@ export const MobileNav: FC<MobileNavProps> = (props) => {
       }))
     : [];
 
+  const [showNavContent, setShowNavContent] = useState<boolean>(false);
+
+  const toggleNavContent = () => {
+    console.log("Toggled nav content");
+    setShowNavContent((prev) => !prev);
+  };
+
   return (
-    <div>
-      <Image {...props.logo} />
-      {hasExpandableItems && (
-        <Accordion id="mobileNavAccordion" items={accordionItems} />
+    <>
+      <div>
+        <Image {...props.logo} />
+        <button onClick={toggleNavContent}>Hello</button>
+      </div>
+      {/* Toggable content */}
+      {showNavContent && (
+        <div>
+          <SearchBar placeholder="TODO searchBar" />
+          {hasExpandableItems && (
+            <Accordion
+              id="mobileNavAccordion"
+              items={accordionItems}
+              variant="primary"
+            />
+          )}
+          {hasLinks &&
+            props.links.map((item) => <h1 key={item._uid}>{item.label}</h1>)}
+        </div>
       )}
-      {hasLinks &&
-        props.links.map((item) => <h1 key={item._uid}>{item.label}</h1>)}
-    </div>
+    </>
   );
 };
