542d1acf619f258fe3d83d745f0bf8ac875b8cf8
Jake Andrews <jake.andrews@ext.ons.gov.uk>
2025-11-06 16:35:56 +0000

    Megamenu overview is rendered above columns

--- src/components/MegaMenu/MegaMenu.tsx
+++ src/components/MegaMenu/MegaMenu.tsx
@@ -1,7 +1,9 @@
 import clsx from "clsx";
 import type { FC } from "react";
 
-import { ListGroupLinks } from "../ListGroup/ListGroup";
+import { TextModule } from "@components/Molecules/Core/TextModule/TextModule";
+import { ListGroupLinks } from "@components/ListGroup/ListGroup";
+
 import type { MegaMenuProps } from "./MegaMenu.interface";
 import styles from "./MegaMenu.module.scss";
 
@@ -9,6 +11,7 @@ export const MegaMenu: FC<MegaMenuProps> = (props) => {
   return (
     <div className={clsx(styles["mega-menu"], "w-100", "py-4")}>
       <div className={clsx("container-lg")}>
+        {props.overview && <TextModule richText={props.overview} />}
         <div className={clsx("row", "row-cols-1", "row-cols-lg-5")}>
           {props.columns.map((column) => (
             <div className={clsx("col", "py-2")} key={column._uid}>
