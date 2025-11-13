f637dfcb25996a7dc5148b76ac8ded153ebd6b13
admilne <andrew.milne@ext.ons.gov.uk>
2025-11-13 09:50:45 +0000

    Add Table component to storybook

---
+++ src/components/Molecules/Core/Table/Table.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import tableData from "./table.json?raw";
+import { Table } from "./Table";
+import type { TableProps } from "./Table.interface";
+
+const meta = {
+  component: Table,
+  title: "Organisms/Core/Table",
+  parameters: {
+    layout: "centeredd",
+  },
+} satisfies Meta<typeof Table>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const TableStory = {
+  name: "Table",
+  args: JSON.parse(tableData) as TableProps,
+} satisfies Story;
