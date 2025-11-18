7e920fe7e11dbe6a200e763fbfe475836add2539
admilne <andrew.milne@ext.ons.gov.uk>
2025-11-18 18:28:21 +0000

    Improve padding around unit overview

--- src/components/Organisms/Unit/UnitOverview/UnitOverview.tsx
+++ src/components/Organisms/Unit/UnitOverview/UnitOverview.tsx
@@ -27,6 +27,7 @@ export const UnitOverview: FC<UnitOverviewProps> = (props) => {
         "border",
         "rounded",
         "p-4",
+        "p-lg-5",
         styles["container-bg"],
       )}
     >
@@ -49,8 +50,8 @@ export const UnitOverview: FC<UnitOverviewProps> = (props) => {
         </div>
       </div>
       <div className={clsx("row")}>
-        <h3 className={clsx("heading-m")}>{props.title}</h3>
-        <hr />
+        <h3 className={clsx("heading-m", "mt-4", "mb-0")}>{props.title}</h3>
+        <hr className={clsx(styles["hr-m"])} />
         <div className={clsx("col-12")}>
           {props.overviewRichText && (
             <TextModule richText={props.overviewRichText} />
