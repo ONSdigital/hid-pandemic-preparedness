2a975adf1407bdc450db5abd0763b13cacc74dad
JonnyAdcockAND <jonathan.adcock@ext.ons.gov.uk>
2025-11-19 11:56:28 +0000

    added new Tip component

---
+++ src/components/Molecules/Core/Tip/Tip.tsx
@@ -0,0 +1,34 @@
+import clsx from "clsx";
+import type { FC } from "react";
+
+import { Icon } from "@src/components/Molecules/Core/Icon/Icon";
+
+import strings from "@src/content/strings.json";
+
+import type { TipProps } from "./Tip.interface";
+import styles from "./Tip.module.scss";
+
+export const Tip: FC<TipProps> = (props) => {
+  const tipStrings = strings.core.tip;
+
+  return (
+    <div
+      className={clsx(
+        "d-flex",
+        "flex-row",
+        "px-4",
+        "py-3",
+        "gap-2",
+        styles["tip-container"],
+      )}
+    >
+      <div>
+        <Icon iconName="RiInformationLine" />
+      </div>
+      <div>
+        <strong>{`${tipStrings.tip}: `}</strong>
+        {props.text}
+      </div>
+    </div>
+  );
+};
