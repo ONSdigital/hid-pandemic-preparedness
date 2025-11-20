ca27ba82c8cfe82aba64c2c531cdddd0e5e3f486
admilne <andrew.milne@ext.ons.gov.uk>
2025-11-20 11:24:48 +0000

    Add Quick links title

--- src/components/Molecules/Unit/Introduction/Introduction.tsx
+++ src/components/Molecules/Unit/Introduction/Introduction.tsx
@@ -2,6 +2,7 @@ import clsx from "clsx";
 import type { FC } from "react";
 
 import type { IntroductionProps } from "./Introduction.interface";
+import strings from "@src/content/strings.json";
 import styles from "./Introduction.module.scss";
 
 export const Introduction: FC<IntroductionProps> = (props) => {
@@ -16,7 +17,7 @@ export const Introduction: FC<IntroductionProps> = (props) => {
       )}
     >
       <div className={clsx("row")}>
-        <h3 className={clsx("heading-m", "mb-0")}>{props.title}</h3>
+        <h4 className={clsx("heading-m", "mb-0")}>{props.title}</h4>
       </div>
       <hr className={clsx(styles["hr-m"])} />
       <div className={clsx("row")}>
@@ -24,6 +25,9 @@ export const Introduction: FC<IntroductionProps> = (props) => {
       </div>
       {props.sectionLinks && (
         <div className={clsx("row")}>
+          <h3 className={clsx("heading-s", "mb-3")}>
+            {strings.unit.quickLinks}
+          </h3>
           <ul className={clsx("ps-5")}>
             {props.sectionLinks.map((link) => (
               <li key={link.id}>
