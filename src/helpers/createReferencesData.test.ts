edcfa4c6c3532f48e87a49cd3c6cf3d2f300e969
JonnyAdcockAND <jonathan.adcock@ext.ons.gov.uk>
2025-11-21 12:31:48 +0000

    finished helper

--- src/helpers/createReferencesData.test.ts
+++ src/helpers/createReferencesData.test.ts
@@ -65,12 +65,25 @@ describe("createReferencesData helper", () => {
     expect(references).toEqual([]);
   });
 
-  test("returns array of `ReferenceProps` items if input story contains references as part of the content", () => {
+  test("returns array of unique `ReferenceProps` items if input story contains references as part of the content", () => {
     // Json story contains reference data
     const referencesStory: ISbStoryData = {
       ...JSON.parse(story).story,
     };
 
+    const references: ReferenceProps[] = createReferencesData(referencesStory);
+    // Story content contains 10 references in today, duplicates should have been removed to return 5
+    expect(references).toHaveLength(5);
+  });
+
+  test("returns array of unique `ReferenceProps` items including new label as a number", () => {
+    // Json story contains reference data
+    const referencesStory: ISbStoryData = {
+      ...JSON.parse(story).story,
+    };
+
+    // References should be unique, and they should include an updated `label` key with an number
+    // corresponding to the index of the reference in the array
     const expectedReferences: ReferenceProps[] = [
       {
         _uid: "60c14c86-23a5-4301-917d-d755f5d7afd1",
@@ -85,6 +98,7 @@ describe("createReferencesData helper", () => {
         yearPublished: "2024",
         _editable:
           '\u003C!--#storyblok#{"name": "Reference", "space": "287525897740819", "uid": "60c14c86-23a5-4301-917d-d755f5d7afd1", "id": "102414283876191"}--\u003E',
+        label: "1",
       },
       {
         _uid: "d2749648-d92c-4b85-8d86-993af8a4715b",
@@ -97,6 +111,7 @@ describe("createReferencesData helper", () => {
         yearPublished: "2025",
         _editable:
           '\u003C!--#storyblok#{"name": "Reference", "space": "287525897740819", "uid": "d2749648-d92c-4b85-8d86-993af8a4715b", "id": "102414283876191"}--\u003E',
+        label: "2",
       },
       {
         _uid: "81701d24-1567-4995-81a1-d9c08fc3016f",
@@ -110,6 +125,7 @@ describe("createReferencesData helper", () => {
         yearPublished: "2020",
         _editable:
           '\u003C!--#storyblok#{"name": "Reference", "space": "287525897740819", "uid": "81701d24-1567-4995-81a1-d9c08fc3016f", "id": "102414283876191"}--\u003E',
+        label: "3",
       },
       {
         _uid: "6de82ce6-830c-4a8a-a138-edac384eebfc",
@@ -123,6 +139,7 @@ describe("createReferencesData helper", () => {
         yearPublished: "2019",
         _editable:
           '\u003C!--#storyblok#{"name": "Reference", "space": "287525897740819", "uid": "6de82ce6-830c-4a8a-a138-edac384eebfc", "id": "102414283876191"}--\u003E',
+        label: "4",
       },
       {
         _uid: "d9df2510-e104-41cc-92e0-0d645e5b9003",
@@ -137,72 +154,7 @@ describe("createReferencesData helper", () => {
         yearPublished: "2021",
         _editable:
           '\u003C!--#storyblok#{"name": "Reference", "space": "287525897740819", "uid": "d9df2510-e104-41cc-92e0-0d645e5b9003", "id": "102414283876191"}--\u003E',
-      },
-      {
-        _uid: "2fad1e62-3bb9-4d77-9675-77b65c8a724d",
-        type: "",
-        component: "Reference",
-        websiteUrl:
-          "https://archive.ourworldindata.org/20250624-125417/grapher/deaths-from-infectious-diseases.html ",
-        accessedDate: "26th August 2025",
-        websiteTitle:
-          "Global Burden of Disease (2024)– with minor processing by Our World in Data. Deaths from infectious diseases [dataset]. In: Global Burden of Disease - Deaths and DALYs [original data]",
-        websiteAuthor: "Institute for Health Metrics and Evaluation (IHME)",
-        yearPublished: "2024",
-        _editable:
-          '\u003C!--#storyblok#{"name": "Reference", "space": "287525897740819", "uid": "2fad1e62-3bb9-4d77-9675-77b65c8a724d", "id": "102414283876191"}--\u003E',
-      },
-      {
-        _uid: "b6abf8b4-0590-482e-8993-f4055984a4fc",
-        type: "",
-        component: "Reference",
-        websiteUrl: "https://data.who.int/dashboards/covid19/deaths",
-        accessedDate: "26th August 2025",
-        websiteTitle: "WHO COVID-19 dashboard",
-        websiteAuthor: "World Health Organisation",
-        yearPublished: "2025",
-        _editable:
-          '\u003C!--#storyblok#{"name": "Reference", "space": "287525897740819", "uid": "b6abf8b4-0590-482e-8993-f4055984a4fc", "id": "102414283876191"}--\u003E',
-      },
-      {
-        _uid: "569cf90b-e8d9-4437-babe-26c3d612eacb",
-        type: "",
-        component: "Reference",
-        websiteUrl:
-          " https://www.worldbank.org/en/publication/poverty-and-shared-prosperity",
-        accessedDate: "26th August 2025",
-        websiteTitle: "Washington, DC: World Bank Group",
-        websiteAuthor: "The World Bank Group. Poverty and Shared Prosperity",
-        yearPublished: "2020",
-        _editable:
-          '\u003C!--#storyblok#{"name": "Reference", "space": "287525897740819", "uid": "569cf90b-e8d9-4437-babe-26c3d612eacb", "id": "102414283876191"}--\u003E',
-      },
-      {
-        _uid: "98497269-d5c8-4f98-afc4-6045b5f159ea",
-        type: "",
-        component: "Reference",
-        websiteUrl:
-          "https://cdn.who.int/media/docs/default-source/consultation-rdb/prioritization-pathogens-v6final.pdf",
-        accessedDate: "26th August 2025",
-        websiteTitle: "Pathogens Prioritization",
-        websiteAuthor: "World Health Organisation",
-        yearPublished: "2019",
-        _editable:
-          '\u003C!--#storyblok#{"name": "Reference", "space": "287525897740819", "uid": "98497269-d5c8-4f98-afc4-6045b5f159ea", "id": "102414283876191"}--\u003E',
-      },
-      {
-        _uid: "03b7c24c-c82e-4513-a567-1680f1911c5e",
-        type: "",
-        component: "Reference",
-        websiteUrl:
-          " https://theindependentpanel.org/wp-content/uploads/2021/05/COVID-19-Make-it-the-Last-Pandemic_final.pdf",
-        accessedDate: "19th August 2025",
-        websiteTitle: "COVID-19: Make it the Last Pandemic",
-        websiteAuthor:
-          "The Independent Panel for Pandemic Preparedness & Response",
-        yearPublished: "2021",
-        _editable:
-          '\u003C!--#storyblok#{"name": "Reference", "space": "287525897740819", "uid": "03b7c24c-c82e-4513-a567-1680f1911c5e", "id": "102414283876191"}--\u003E',
+        label: "5",
       },
     ];
 
--- src/helpers/createReferencesData.ts
+++ src/helpers/createReferencesData.ts
@@ -1,12 +1,51 @@
+import jp from "jsonpath";
+
 import type { ISbStoryData } from "storyblok-js-client";
-import { v4 as uuidv4 } from "uuid";
 
 import type { ReferenceProps } from "@src/components/Molecules/Core/Reference/Reference.interface";
 
 // Function extracts all `Reference` component data from input `story` and returns as an array of
 // `ReferenceProps` objects including numbered label
 export function createReferencesData(story: ISbStoryData): ReferenceProps[] {
-  let returnReferenceProps: ReferenceProps[] = [];
+  // console.log(story.content);
+  const TARGET_MATCH_KEYS: string[] = [
+    "accessedDate",
+    "yearPublished",
+    "websiteAuthor",
+    "websiteTitle",
+    "websiteUrl",
+  ];
+  let returnReferences: ReferenceProps[] = [];
 
-  return returnReferenceProps;
+  // Find any references as part of the content data
+  const references: ReferenceProps[] = jp.query(
+    story.content,
+    "$..[?(@.component=='Reference')]",
+  );
+  // console.log(references);
+
+  if (references) {
+    // Remove any duplicates that might be present based on a subset of keys
+    const duplicatesSet = new Set();
+
+    const uniqueReferences = references.filter((item) => {
+      // Create a composite key string from the specified keys
+      const compositeKey = TARGET_MATCH_KEYS.map((key) => item[key]).join("|");
+
+      if (duplicatesSet.has(compositeKey)) {
+        return false; // duplicate, filter out
+      } else {
+        duplicatesSet.add(compositeKey);
+        return true; // first occurrence, keep it
+      }
+    });
+
+    // Update to add additional `label` key for numbering
+    returnReferences = uniqueReferences.map((ref, index) => ({
+      ...ref,
+      label: (index + 1).toString(),
+    }));
+  }
+
+  return returnReferences;
 }
