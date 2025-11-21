896bea2ebd6fb0a8b8bf302af1a2c5061685ca04
JonnyAdcockAND <jonathan.adcock@ext.ons.gov.uk>
2025-11-21 14:41:56 +0000

    added updateStoryReferences helper to add labels to story references

--- src/helpers/updateReferences.ts
+++ src/helpers/updateReferences.ts
@@ -4,17 +4,19 @@ import type { ISbStoryData } from "storyblok-js-client";
 
 import type { ReferenceProps } from "@src/components/Molecules/Core/Reference/Reference.interface";
 
+// List of `ReferenceProps` keys we target to match a reference. If all values associated with these
+// keys are the same, the reference is a duplicate
+const TARGET_MATCH_KEYS: string[] = [
+  "accessedDate",
+  "yearPublished",
+  "websiteAuthor",
+  "websiteTitle",
+  "websiteUrl",
+];
+
 // Function extracts all `Reference` component data from input `story` and returns as an array of
 // `ReferenceProps` objects including numbered label
 export function createReferencesData(story: ISbStoryData): ReferenceProps[] {
-  // console.log(story.content);
-  const TARGET_MATCH_KEYS: string[] = [
-    "accessedDate",
-    "yearPublished",
-    "websiteAuthor",
-    "websiteTitle",
-    "websiteUrl",
-  ];
   let returnReferences: ReferenceProps[] = [];
 
   // Find any references as part of the content data
@@ -22,7 +24,6 @@ export function createReferencesData(story: ISbStoryData): ReferenceProps[] {
     story.content,
     "$..[?(@.component=='Reference')]",
   );
-  // console.log(references);
 
   if (references) {
     // Remove any duplicates that might be present based on a subset of keys
@@ -48,4 +49,48 @@ export function createReferencesData(story: ISbStoryData): ReferenceProps[] {
   }
 
   return returnReferences;
+}
+
+// Function updates references in `story` with those in `referenceData` if valid. This allows us to
+// set the label for each reference globally across the story
+export function updateStoryReferences(
+  story: ISbStoryData,
+  referenceData: ReferenceProps[],
+): ISbStoryData {
+  const returnedStory = structuredClone(story);
+
+  // If we have some references to perform an update with, create a map
+  if (referenceData.length > 0) {
+    // Create a Map for quick lookup of input references by composite key
+    const referenceDataMap = new Map();
+
+    referenceData.forEach((ref) => {
+      const compositeKey = TARGET_MATCH_KEYS.map((key) => ref[key]).join("|");
+      referenceDataMap.set(compositeKey, ref);
+    });
+
+    // Find any existing references in the story content
+    const existingReferences = jp.nodes(
+      returnedStory.content,
+      "$..[?(@.component=='Reference')]",
+    );
+
+    // If there any existing references to update, do so
+    if (existingReferences.length > 0) {
+      // Loop through these nodes and update them if we find a matching reference in `referenceData`
+      existingReferences.map(({ value }) => {
+        // Create a composite key string from the specified keys to find matching reference
+        const compositeKey = TARGET_MATCH_KEYS.map((key) => value[key]).join(
+          "|",
+        );
+
+        const matchingRef = referenceDataMap.get(compositeKey);
+        if (matchingRef) {
+          // Add or update label key
+          value.label = matchingRef.label;
+        }
+      });
+    }
+  }
+  return returnedStory;
 }
