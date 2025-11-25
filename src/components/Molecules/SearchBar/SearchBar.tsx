165b90f0f7a3ea4c50be0083083eef165a4d897a
Patrick Holden <holden.patrick@gmail.com>
2025-11-25 09:32:32 +0000

    do not  force autofocus on search input

--- src/components/Molecules/SearchBar/SearchBar.tsx
+++ src/components/Molecules/SearchBar/SearchBar.tsx
@@ -155,7 +155,6 @@ export const SearchBar: FC<SearchBarProps> = (props) => {
       <div className={clsx("input-group", "mb-3")}>
         <input
           aria-label={props.placeholder}
-          autoFocus={props.isInline}
           className={clsx("form-control", styles["input-sizing"])}
           onChange={onChange}
           onFocus={handleFocus}
