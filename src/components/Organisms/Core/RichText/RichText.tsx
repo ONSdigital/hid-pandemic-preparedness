1d39be926fd62019f90c49ef78a4b416520b50cb
admilne <andrew.milne@ext.ons.gov.uk>
2025-11-28 16:43:35 +0000

    Fix prettier issue

--- src/components/Organisms/Core/RichText/RichText.tsx
+++ src/components/Organisms/Core/RichText/RichText.tsx
@@ -3,7 +3,7 @@ import type { FC } from "react";
 
 import { TextModule } from "@/src/components/Molecules/Core/TextModule/TextModule";
 
-import type { RichTextProps } from "./RichText.interface"
+import type { RichTextProps } from "./RichText.interface";
 
 export const RichText: FC<RichTextProps> = (props) => {
   return (
