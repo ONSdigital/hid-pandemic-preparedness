c40dcfd2e051772b3244d3169ab221255a03735d
Jake Andrews <jake.andrews@ext.ons.gov.uk>
2025-12-03 16:37:22 +0000

    Create reusable button component

---
+++ src/components/Molecules/Core/IconButton/IconButton.tsx
@@ -0,0 +1,29 @@
+import clsx from "clsx";
+import type { FC } from "react";
+
+import { Icon } from "@components/Molecules/Core/Icon/Icon";
+
+import type { IconButtonProps } from "./IconButton.interface";
+
+export const IconButton: FC<IconButtonProps> = (props) => {
+  const href = props.isDownloadable
+    ? (props.downloadableContent?.filename ?? undefined)
+    : props.link?.url;
+
+  return (
+    <a
+      href={href}
+      className={clsx(
+        "btn",
+        "btn-primary",
+        "d-inline-flex",
+        "align-items-center",
+        "gap-2",
+      )}
+      download={props.isDownloadable}
+    >
+      {props.buttonText}
+      {props.icon && <Icon iconName={props.icon} className={clsx("ms-2")} />}
+    </a>
+  );
+};
