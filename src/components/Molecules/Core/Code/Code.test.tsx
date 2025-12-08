5c4c9a2424d5f5aa073317905036f9ae895a2c82
admilne <andrew.milne@ext.ons.gov.uk>
2025-12-08 12:02:01 +0000

    Change case for code.json file

--- src/components/Molecules/Core/Code/Code.test.tsx
+++ src/components/Molecules/Core/Code/Code.test.tsx
@@ -1,7 +1,7 @@
 import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { Code } from "./Code";
-import mockData from "./Code.json?raw";
+import mockData from "./code.json?raw";
 
 const defaultProps = JSON.parse(mockData);
 
