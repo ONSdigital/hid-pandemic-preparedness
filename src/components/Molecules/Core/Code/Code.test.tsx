0c0c97d272939d8f0f649c66877a5cce528dba72
admilne <andrew.milne@ext.ons.gov.uk>
2025-12-08 11:47:02 +0000

    Add dummy content and tests for Code component

---
+++ src/components/Molecules/Core/Code/Code.test.tsx
@@ -0,0 +1,14 @@
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { Code } from "./Code";
+import mockData from "./Code.json?raw";
+
+const defaultProps = JSON.parse(mockData);
+
+describe("Code", () => {
+  it("renders without crashing", () => {
+    render(<Code {...defaultProps} />);
+    expect(screen.getByText(defaultProps.title)).toBeDefined();
+    expect(screen.getByText(defaultProps.languages[0].language)).toBeDefined();
+  });
+});
