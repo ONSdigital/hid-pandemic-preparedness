803fce00c9618740e8e4cf9bf8168e9c5c39954d
Jake Andrews <jake.andrews@ext.ons.gov.uk>
2025-12-09 14:59:58 +0000

    ContentHighlight as a react component

---
+++ src/components/Molecules/Core/ContentHighlight/ContentHighlight.interface.tsx
@@ -0,0 +1,3 @@
+import type { ContentHighlight } from "@/src/types/bloks/storyblok-components";
+
+export interface ContentHighlightProps extends ContentHighlight {}
---
+++ src/components/Molecules/Core/ContentHighlight/ContentHighlight.stories.tsx
@@ -0,0 +1,43 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { ContentHighlight } from "./ContentHighlight";
+import contentHighlightData from "./ContentHighlight.json?raw";
+
+const meta = {
+  component: ContentHighlight,
+  title: "Molecules/Core/ContentHighlight",
+  parameters: {
+    layout: "centered",
+  },
+  argTypes: {
+    _editable: {
+      table: {
+        disable: true,
+      },
+    },
+    _uid: {
+      table: {
+        disable: true,
+      },
+    },
+
+    component: {
+      table: {
+        disable: true,
+      },
+    },
+    textarea: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+} satisfies Meta<typeof ContentHighlight>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const ContentHighlightStory = {
+  name: "ContentHighlight",
+  args: JSON.parse(contentHighlightData),
+} satisfies Story;
