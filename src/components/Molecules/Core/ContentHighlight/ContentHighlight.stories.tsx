93085154a77690cbf91f2933c4a50896b44e3c69
Jake Andrews <jake.andrews@ext.ons.gov.uk>
2025-12-11 09:51:07 +0000

    Update import for contentHighlight within ContentHighlight storybook file

--- src/components/Molecules/Core/ContentHighlight/ContentHighlight.stories.tsx
+++ src/components/Molecules/Core/ContentHighlight/ContentHighlight.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
 import { ContentHighlight } from "./ContentHighlight";
-import contentHighlightData from "./ContentHighlight.json?raw";
+import contentHighlightData from "./contentHighlight.json?raw";
 
 const meta = {
   component: ContentHighlight,
